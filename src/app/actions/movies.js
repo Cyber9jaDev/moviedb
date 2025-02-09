e55bbe05b52e5106a950acd5b482e8b2622b7aa4
cyber9ja <ayodejioladapo15@gmail.com>
2025-02-09 10:51:52 +0000

    movies

--- src/app/actions/movies.js
+++ src/app/actions/movies.js
@@ -1,4 +1,4 @@
-export async function getMovies (){
+export async function movieLists (){
   const url = 'https://api.themoviedb.org/3/movie/now_playing';
   const options = {
     method: 'GET',
@@ -17,12 +17,12 @@ export async function getMovies (){
 
 export async function singleMovie(id){
   const url = `https://api.themoviedb.org/3/movie/${id}?language=en-US`;
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_API_KEY}`
-  }
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_API_KEY}`
+    }
 };
 
   const results = await fetch(url, options);
--- src/app/page.js
+++ src/app/page.js
@@ -1,21 +1,21 @@
 import Link from "next/link";
-import { fetchMovies } from "./actions";
+import { movieLists } from "./actions/movies";
 
 export default async function Home() {
-  const movies = await fetchMovies(); // Fetch movies on the server
+  const movies = await movieLists(); // Fetch movies on the server
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <h1 className="text-3xl font-bold text-center mb-6">Popular Movies</h1>
+      <h1 className="text-3xl font-bold text-center mb-6">Movies</h1>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {movies.map((movie) => (
-          <Link key={movie.id} href={`/movie/${movie.id}`} className="block">
+          <Link key={movie.title} href={`/movie/${movie.id}`} className="block">
             <div className="bg-gray-800 text-white p-4 rounded-lg shadow-lg hover:scale-105 transition">
-              {/* <img
+              <img
                 src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`}
                 alt={movie.title}
                 className="w-full h-auto rounded-lg"
-              /> */}
+              />
               <h2 className="text-lg font-semibold mt-2">{movie.title}</h2>
               <p className="text-gray-400 text-sm">⭐ {movie.vote_average}</p>
             </div>
