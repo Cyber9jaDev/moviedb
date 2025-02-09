a50c84f543bb5b5a79e5369632653ea0d5545505
cyber9ja <ayodejioladapo15@gmail.com>
2025-02-09 11:54:04 +0000

    ...

--- src/app/page.js
+++ src/app/page.js
@@ -7,7 +7,7 @@ export default async function Home() {
   return (
     <div className="container mx-auto px-4 py-8">
       <h1 className="text-3xl font-bold text-center mb-6">Movies</h1>
-      <div className="grid grid-cols-1 min-[540px]:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 min-[540px]:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {movies.map((movie) => (
           <Link key={movie.title} href={`/movie/${movie.id}`} className="block">
             <div className="bg-gray-800 text-white p-4 rounded-lg shadow-lg hover:scale-105 transition">
