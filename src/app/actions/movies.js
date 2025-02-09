0c90b5b143daa9a3d1645157b661cf44f6d08999
cyber9ja <ayodejioladapo15@gmail.com>
2025-02-09 10:40:47 +0000

    first commit

--- src/app/actions/movies.js
+++ src/app/actions/movies.js
@@ -1,3 +1,32 @@
-export async function (){
-  
+export async function getMovies (){
+  const url = 'https://api.themoviedb.org/3/movie/now_playing';
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_API_KEY}`
+    }
+  };
+
+  const results = await fetch(url, options);
+  const movies = await results.json();
+
+  return movies.results
+}
+
+
+export async function singleMovie(id){
+  const url = `https://api.themoviedb.org/3/movie/${id}?language=en-US`;
+const options = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_API_KEY}`
+  }
+};
+
+  const results = await fetch(url, options);
+  const movies = await results.json()
+
+  return movies.results
 }
